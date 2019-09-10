4fc25ed45ad09baa6dddf72a91b1957b3a1a89bc
Alexey Rusov <rolic402@mail.ru>
2019-09-10 10:18:42 +0300

    Formal comment added

--- bufpool.go
+++ bufpool.go
@@ -11,6 +11,7 @@ var bufPool = sync.Pool{New: func() interface{} { return new(bytes.Buffer) }}
 
 //----------------------------------------------------------------------------------------------------------------------------//
 
+// GetBuf --
 func GetBuf() *bytes.Buffer {
 	if bb, ok := bufPool.Get().(*bytes.Buffer); ok {
 		return bb
@@ -18,6 +19,7 @@ func GetBuf() *bytes.Buffer {
 	return new(bytes.Buffer)
 }
 
+// PutBuf --
 func PutBuf(b *bytes.Buffer) {
 	b.Reset()
 	bufPool.Put(b)
